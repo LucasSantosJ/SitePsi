823262b0f174f2b86d7f1040284806319aa2fb05
Lucas Santos <lucassi.2022.2@gmail.com>
2025-04-02 11:51:43 -0300

    correçao de todo arquivo

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('/menu.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('menu').innerHTML = data)
+        .catch(error => console.error('Erro ao carregar o menu:', error));
+});
+
+function abrirWhatsApp(servico) {
+        let numero = "5582987729579"; 
+        let mensagem = encodeURIComponent(`Olá, gostaria de agendar uma consulta para ${servico}. Poderia me passar mais informações?`);
+        let link = `https://wa.me/${numero}?text=${mensagem}`;
+        window.open(link, "_blank");
+ }
+

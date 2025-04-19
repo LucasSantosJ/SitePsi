04336b30db4b63f0bdcd8dadeb5295ba94c72820
Lucas Santos <lucassi.2022.2@gmail.com>
2025-04-19 13:17:20 -0300

    adicionando responsividade

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/menu.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('menu').innerHTML = data)
-        .catch(error => console.error('Erro ao carregar o menu:', error));
+window.addEventListener('scroll', revelarAoRolar);
+window.addEventListener('load', revelarAoRolar); // ativa no carregamento também
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('menu.html')
+      .then(response => response.text())
+      .then(data => {
+          document.body.insertAdjacentHTML('afterbegin', data);
+          
+          const hamburger = document.getElementById('hamburger');
+          const menu = document.getElementById('menu');
+          
+          if (hamburger && menu) {
+              hamburger.addEventListener('click', function() {
+                  menu.classList.toggle('ativo');
+                  hamburger.classList.toggle('ativo');
+                  
+                  // Bloqueia scroll quando menu está aberto
+                  document.body.style.overflow = menu.classList.contains('ativo') ? 'hidden' : 'auto';
+              });
+              
+              // Fecha menu ao clicar nos links
+              document.querySelectorAll('.menu a').forEach(link => {
+                  link.addEventListener('click', () => {
+                      menu.classList.remove('ativo');
+                      hamburger.classList.remove('ativo');
+                      document.body.style.overflow = 'auto';
+                  });
+              });
+          }
+      })
+      .catch(error => console.error('Erro ao carregar o menu:', error));
 });
 
 function abrirWhatsApp(servico) {
@@ -16,3 +43,22 @@ function abrirWhatsApp(servico) {
     const url = "https://www.instagram.com/psicologajacy/?igsh=cHMyamxjN2QxYmNz&utm_source=qr#"; 
     window.open(url, "_blank");
 }
+
+
+function revelarAoRolar() {
+  const elementos = document.querySelectorAll('.scroll-reveal');
+  const topoTela = window.innerHeight;
+
+  elementos.forEach(el => {
+    const distanciaTopo = el.getBoundingClientRect().top;
+
+    if (distanciaTopo < topoTela - 100) {
+      el.classList.add('mostrado');
+    }
+  });
+}
+
+
+
+  
+
